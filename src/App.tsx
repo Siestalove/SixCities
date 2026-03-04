56056fafcac28f1ed4449f19467874815c604ecc
Siestalove <willovv4u@gmail.com>
2026-03-04 20:24:18 +0700

    Added 4 pages and 1 component
    Добавил 4 страницы и 1 компонент
    Также на главной странице добавил  пропсы, данные из которых берутся в ```index.tsx``` прокидываются в ```App.tsx```, которые дальше прокидываются в ```MainPage.tsx```

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import MainPage from './pages/MainPage';
+
+type AppProps = {
+  offersCount: number;
+}
+
+export default function App({ offersCount } : AppProps) {
+  return (
+    <MainPage
+      offersCount = {offersCount}
+    >
+    </MainPage>
+  );
+}
